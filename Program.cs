26f85945c650cb41d6ec2e3b2398091e67ed94dc
chedva3434 <ch0534194892@gmail.com>
2025-02-03 23:02:58 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+using TodoApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  
+              .AllowAnyMethod()  
+              .AllowAnyHeader(); 
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();  
+builder.Services.AddSwaggerGen(); 
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"),
+                     Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
+
+builder.Services.AddDbContext<ToDoDbContext>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.RoutePrefix = string.Empty;  
+    });
+}
+
+app.UseCors("AllowAll");
+
+app.MapGet("/items", (ToDoDbContext db) => db.Items.ToList());
+
+app.MapGet("/items/{id}", (ToDoDbContext context, int id) =>
+{
+    var item = context.Items.FirstOrDefault(i => i.Id == id);
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(item); 
+});
+
+app.MapPost("/items", (ToDoDbContext db, Item newItem) =>
+{
+    db.Items.Add(newItem);
+    db.SaveChanges(); 
+    return Results.Created($"/items/{newItem.Id}", newItem);
+});
+
+app.MapPut("/items/{id}", (ToDoDbContext db, int id, Item updatedItem) =>
+{
+    var item = db.Items.FirstOrDefault(i => i.Id == id);
+    if (item == null)
+    {
+        return Results.NotFound(); 
+    }
+
+    item.Name = updatedItem.Name; 
+    db.SaveChanges();
+    return Results.Ok(item); 
+});
+
+app.MapDelete("/items/{id}", (ToDoDbContext db, int id) =>
+{
+    var item = db.Items.FirstOrDefault(i => i.Id == id);
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Items.Remove(item); 
+    db.SaveChanges(); 
+    return Results.Ok(); 
+});
+
+app.Run();
